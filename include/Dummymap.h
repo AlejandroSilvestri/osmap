a122771ab87bc3bab19a6439cbfb6c935dcbba80
Alejandro Silvestri <AlejandroSilvestri@gmail.com>
2019-04-05 08:05:46 -0300

    GPL license
    GPL v3 license text added to a couple of files that were missing it.

--- include/Dummymap.h
+++ include/Dummymap.h
@@ -1,9 +1,22 @@
-/*
- * dummymap.h
- *
- *  Created on: 17 nov. 2018
- *      Author: alejandro
- */
+/**
+* This file is part of OSMAP.
+*
+* Copyright (C) 2018-2019 Alejandro Silvestri <alejandrosilvestri at gmail>
+* For more information see <https://github.com/AlejandroSilvestri/osmap>
+*
+* OSMAP is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* OSMAP is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with OSMAP. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef DUMMYMAP_H_
 #define DUMMYMAP_H_
